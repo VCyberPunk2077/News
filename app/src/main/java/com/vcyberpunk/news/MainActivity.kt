ff27ca9b810ecef9121bb1621253bcf5321c765b
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-06-26 18:45:53 +0300

    Initial commit

---
+++ app/src/main/java/com/vcyberpunk/news/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.vcyberpunk.news
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.vcyberpunk.news.ui.theme.NewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsTheme {
+
+            }
+        }
+    }
+}
