2cdcc05088e99ac360c199b2129690352f9fedc6
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-06-28 19:06:46 +0300

    feat: add NewsApiKeyInterceptor

---
+++ app/src/main/java/com/vcyberpunk/news/data/remote/api/NewsApiKeyInterceptor.kt
@@ -0,0 +1,29 @@
+package com.vcyberpunk.news.data.remote.api
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+import com.vcyberpunk.news.BuildConfig
+
+class NewsApiKeyInterceptor @Inject constructor(): Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        val newUrl = originalRequest.url
+            .newBuilder()
+            .addQueryParameter(KEY_PARAM, BuildConfig.NEWS_API_KEY)
+            .build()
+
+        val newRequest = originalRequest
+            .newBuilder()
+            .url(newUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+
+    private companion object {
+        const val KEY_PARAM = "key"
+    }
+}
