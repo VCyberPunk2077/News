20d365dbd38ee64b6ad9ae8247f6cf5c2fc16231
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-07-02 02:15:29 +0300

    feat: add SettingsMapper

---
+++ app/src/main/java/com/vcyberpunk/news/data/mapper/SettingsMapper.kt
@@ -0,0 +1,7 @@
+package com.vcyberpunk.news.data.mapper
+
+import com.vcyberpunk.news.domain.entity.Interval
+
+fun Int.toInterval(): Interval {
+    return Interval.entries.firstOrNull { it.minutes == this} ?: Interval.MIN_15
+}
