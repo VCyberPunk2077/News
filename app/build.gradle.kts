3aed89ce307166dcb2c8584ae7e66543b519e23d
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-07-02 02:14:40 +0300

    build: add DataStore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.hilt.work)
     ksp(libs.androidx.hilt.compiler)
     implementation(libs.androidx.work.runtime.ktx)
