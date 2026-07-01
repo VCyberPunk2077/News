6ef0303429d29ddc9060b2b33c8c50ca9b6479f8
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-07-01 18:04:16 +0300

    feat: add implicit intents for article

--- app/src/main/java/com/vcyberpunk/news/presentation/screen/subscriptions/SubscriptionsScreen.kt
+++ app/src/main/java/com/vcyberpunk/news/presentation/screen/subscriptions/SubscriptionsScreen.kt
@@ -1,5 +1,7 @@
 package com.vcyberpunk.news.presentation.screen.subscriptions
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -33,12 +35,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
@@ -286,6 +290,7 @@ private fun Subscriptions(
 @Composable
 private fun ArticleCard(
     modifier: Modifier = Modifier,
+    context: Context = LocalContext.current,
     article: Article
 ) {
     Card(
@@ -355,7 +360,11 @@ private fun ArticleCard(
         ) {
             Button(
                 modifier = Modifier.weight(1f),
-                onClick = {}
+                onClick = {
+                    val uri = article.url.toUri()
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    context.startActivity(intent)
+                }
             ) {
                 Icon(
                     imageVector = CustomIcons.OpenInNew,
@@ -369,7 +378,13 @@ private fun ArticleCard(
 
             Button(
                 modifier = Modifier.weight(1f),
-                onClick = {}
+                onClick = {
+                    val intent = Intent(Intent.ACTION_SEND).apply {
+                        type = "plain/text"
+                        putExtra(Intent.EXTRA_TEXT, "${article.title}\n\n${article.url}")
+                    }
+                    context.startActivity(intent)
+                }
             ) {
                 Icon(
                     imageVector = CustomIcons.Share,
