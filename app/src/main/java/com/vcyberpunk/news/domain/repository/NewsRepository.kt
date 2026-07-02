ec2ee84c3af45055e6e31ea33751a624f6cf50ed
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-07-02 03:04:04 +0300

    feat: add startBackgroundRefresh method to repository

--- app/src/main/java/com/vcyberpunk/news/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/vcyberpunk/news/domain/repository/NewsRepository.kt
@@ -1,6 +1,7 @@
 package com.vcyberpunk.news.domain.repository
 
 import com.vcyberpunk.news.domain.entity.Article
+import com.vcyberpunk.news.domain.entity.RefreshConfig
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
@@ -18,5 +19,7 @@ interface NewsRepository {
     fun getArticlesByTopics(topics: List<String>): Flow<List<Article>>
 
     suspend fun clearAllArticles(topics: List<String>)
+
+    fun startBackgroundRefresh(refreshConfig: RefreshConfig)
 
 }
