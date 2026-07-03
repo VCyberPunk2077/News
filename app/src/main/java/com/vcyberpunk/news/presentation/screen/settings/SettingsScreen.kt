f6c2fbac013173d956e213e23fbd4ea1cade42a9
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-07-03 23:05:02 +0300

    fix: change icon shape

--- app/src/main/java/com/vcyberpunk/news/presentation/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/vcyberpunk/news/presentation/screen/settings/SettingsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
@@ -32,6 +33,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -63,6 +65,7 @@ fun SettingsScreen(
                     Icon(
                         modifier = Modifier
                             .padding(horizontal = 16.dp)
+                            .clip(CircleShape)
                             .clickable {
                                 onBackClick()
                             },
