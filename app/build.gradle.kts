ebbc07122675fff975ddff4b631dc9b2cb52861e
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-07-03 23:00:08 +0300

    fix: add dependency to successfully project building

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,5 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+    implementation(libs.error.prone.annotations)
 }
